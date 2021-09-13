8bd8134c6894dbf65aa7792a2e7b8e1194708402
Luis Balladares <luisdavidballadares@gmail.com>
2021-09-13 18:00:02 -0300

    token expiration added

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core'
 import { Link, useHistory, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-
+import decode from 'jwt-decode'
 
 import useStyles from './styles'
 import memories from '../../images/memories.png'
@@ -25,6 +25,12 @@ const NavBar = () => {
     useEffect(() => {
         const token = user?.token
         //check JWT json web token
+        if(token){
+            const decodedToken = decode(token)
+            if(decodedToken.exp * 1000 < new Date().getTime()){logout()}
+        }
+        
+
         setUser(JSON.parse(localStorage.getItem('profile')))
     }, [location])
     
